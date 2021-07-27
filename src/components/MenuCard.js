53ee3f78d1cdcffa378aa44c3bf8ed285859840c
priyanshu <sinhapriyanshu0312@gmail.com>
2021-07-27 21:06:39 +0530

    a

--- src/components/MenuCard.js
+++ src/components/MenuCard.js
@@ -2,26 +2,26 @@ import React from "react";
 
 const MenuCard = ({ menuData }) => {
   //   console.log(menuData);
+
   return (
     <div>
       <section className="main-card--cointainer">
         {menuData.map((curElem) => {
+          const { id, name, description, category, image } = curElem;
           return (
             <>
-              <div className="card-container" key={curElem.id}>
+              <div className="card-container" key={id}>
                 <div className="card">
                   <div className="card-body">
                     <span className="card-number card-circle subtle">1</span>
-                    <span className="card-author subtle">
-                      {curElem.category}
-                    </span>
-                    <h6 className="card-title">{curElem.name}</h6>
+                    <span className="card-author subtle">{category}</span>
+                    <h6 className="card-title">{name}</h6>
                     <span className="card-description subtle">
-                      {curElem.description}
+                      {description}
                     </span>
                     <div className="card-read">Read</div>
                   </div>
-                  <img src={curElem.image} alt="" className="card-media" />
+                  <img src={image} alt="" className="card-media" />
                   <span className="card-tag subtle">Order Now</span>
                 </div>
               </div>
--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -2,14 +2,35 @@ import React, { useState } from "react";
 import "./style.css";
 import Menu from "./menuApi";
 import MenuCard from "./MenuCard";
+import Navbar from "./Navbar";
 
+const uniqueList = [
+  ...new Set(
+    Menu.map((curElem) => {
+      return curElem.category;
+    })
+  ),
+  "All",
+];
+console.log(uniqueList);
 const Restaurant = () => {
   const [menuData, setMenuData] = useState(Menu);
+  const [menuList, setMenuList] = useState(uniqueList);
 
-
+  const filterItem = (category) => {
+    if (category === "All") {
+      setMenuData(Menu);
+      return;
+    }
+    const updatedList = Menu.filter((curElem) => {
+      return curElem.category === category;
+    });
+    setMenuData(updatedList);
+  };
   return (
     <>
-      <MenuCard  menuData={menuData}/>
+      <Navbar filterItem={filterItem} menuList={menuList} />
+      <MenuCard menuData={menuData} />
     </>
   );
 };
